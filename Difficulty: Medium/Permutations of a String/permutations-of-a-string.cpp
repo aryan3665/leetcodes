2d2cd66b7cbbecb89a6fd60c2c911c143c644406
aryan3665 <aryantripathi16022006@gmail.com>
2025-01-28 20:02:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -7,21 +7,28 @@ using namespace std;
 
 
 class Solution {
-  public:
+public:
+    void per(string &s, vector<string> &ans, int index) {
+        if (index == s.size()) {
+            ans.push_back(s);
+            return;
+        }
+
+        for (int i = index; i < s.size(); i++) {
+            swap(s[i], s[index]); // Swap the current index with the loop index
+            per(s, ans, index + 1); // Recur for the next index
+            swap(s[i], s[index]); // Backtrack
+        }
+    }
+
     vector<string> findPermutation(string &s) {
         vector<string> ans;
-        
-        string str = s;
-        
-        sort(str.begin(),str.end());
-        
-        do{
-            ans.push_back(str);
-        } while(next_permutation(str.begin(),str.end()));
+        per(s, ans, 0);
+        sort(ans.begin(), ans.end()); // Optional: Sort to have permutations in lexicographical order
+        ans.erase(unique(ans.begin(), ans.end()), ans.end()); // Optional: Remove duplicates
         return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 int main() {
