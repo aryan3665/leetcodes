070d46b2d11bd4f5e3b3e1aabca5d088c2007a30
aryan3665 <aryantripathi16022006@gmail.com>
2025-02-27 18:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (5.02%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int n=word.size();
-        char a=word[0];
-        char b=word[n-1];
-        if((a>='A'&&a<='Z')&&(b>='A'&&b<='Z')){
-            for(int i=1;i<n-1;i++){
-                if(word[i]>='a'&&word[i]<='z'){
-                    return false;
-                }
-            }
-        }
-        else if((a>='a'&&a<='z')&&(b>='a'&&b<='z')){
+        int upperCount = 0, n = word.size();
 
-             for(int i=1;i<n-1;i++){
-                if(word[i]>='A'&&word[i]<='Z'){
-                    return false;
-                }
-            }
-
+        for (char c : word) {
+            if (isupper(c)) upperCount++;
         }
 
-        else if((a>='A'&&a<='Z')&&(b>='a'&&b<='z')){
-             for(int i=1;i<n-1;i++){
-                if(word[i]>='A'&&word[i]<='Z'){
-                    return false;
-                }
-            }
-        }
-
-        else if((a>='a'&&a<='z')&&(b>='A'&&b<='Z')){
-            return false;
-        }
-
-        return true;
+        // Valid cases:
+        // 1. All uppercase (upperCount == n)
+        // 2. All lowercase (upperCount == 0)
+        // 3. Only the first letter is uppercase (upperCount == 1 && isupper(word[0]))
+        return (upperCount == n || upperCount == 0 || (upperCount == 1 && isupper(word[0])));
     }
 };
