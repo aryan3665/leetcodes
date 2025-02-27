4da76185669816d73f14f50ddeb18d9b65c563fc
aryan3665 <aryantripathi16022006@gmail.com>
2025-02-27 18:13:10 +0530

    Time: 130 ms (84.01%) | Memory: 145.4 MB (86.34%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-         sort(changed.begin(), changed.end());  
+       if (changed.size() % 2 != 0) return {}; // If the length is odd, return empty
+
+    sort(changed.begin(), changed.end()); // Sort the array
+    unordered_map<int, int> freq; // Frequency map
     vector<int> original;
-    
-    while (!changed.empty()) {
-        int num = changed[0];  
-        auto it = find(changed.begin() + 1, changed.end(), num * 2);  // Find its double
-        
-        if (it != changed.end()) {  // If found
-            original.push_back(num);
-            changed.erase(it);  // Remove the double
-            changed.erase(changed.begin());  // Remove the original number
-        } else {
-            return {};  // If no valid pair is found, return empty array
-        }
+
+    // Count the frequency of each element
+    for (int num : changed) {
+        freq[num]++;
     }
-    
+
+    // Process each element
+    for (int num : changed) {
+        if (freq[num] == 0) continue; // Skip if already used
+        if (freq[num * 2] == 0) return {}; // If double not found, return empty
+
+        original.push_back(num);
+        freq[num]--;      // Decrease frequency of the number
+        freq[num * 2]--;  // Decrease frequency of its double
+    }
+
     return original;
     }
 };
