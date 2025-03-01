51ac79dd181b34ec92e4fa937756d7e8f9e8f6a3
aryan3665 <aryantripathi16022006@gmail.com>
2025-03-01 16:16:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (23.58%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
-                nums[i]=nums[i]*2;
-                nums[i+1]=0;
+        int n = nums.size();
+        
+        // Step 1: Modify array as per the given condition
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
             }
         }
-        vector<int>ans;
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-            ans.push_back(nums[i]);
+        
+        
+        int index = 0; 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[index]);
+                index++;
             }
-            else{
-                c++;
-            }
-        }
-        for(int i=0;i<c;i++){
-                ans.push_back(0);
-            
         }
 
-        return ans;
+        return nums;
     }
 };
