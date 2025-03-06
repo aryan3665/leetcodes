f3d90eaf37b59574b26dbc23ca5f5ec8ff54c540
aryan3665 <aryantripathi16022006@gmail.com>
2025-03-06 09:50:54 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (97.94%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         vector<int> ans(2);
-        map<int, int> mp;
+       
         int n = grid.size();
+        long long N=n*n;
 
-        // Count occurrences of each number in the grid
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mp[grid[i][j]]++;
-            }
+    long long gridsum=0;
+    long long gridsqsum=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            gridsum+=grid[i][j];
+            gridsqsum+=grid[i][j]*grid[i][j];
         }
+    }
+    long long exsum=(N*(N+1))/2;
+    long long exsqsum=(N*(N+1)*(2*N+1))/6;
+    int sqdiff=gridsqsum-exsqsum;
+    int sumdiff=gridsum-exsum;
+    int a=(sqdiff/sumdiff+sumdiff)/2;
+    int b=(sqdiff/sumdiff-sumdiff)/2;
+    ans[0]=a;
+    ans[1]=b;
 
-        // Find the repeated and missing number
-        for (int i = 1; i <= n * n; i++) {  // Iterate from 1 to n*n
-            if (mp[i] == 2) {
-                ans[0] = i;  // Repeated number
-            }
-            if (mp[i] == 0) {
-                ans[1] = i;  // Missing number
-            }
-        }
 
         return ans;
     }
