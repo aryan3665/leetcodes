140b4614d837c4e4456c84c61ba78c427da523d7
aryan3665 <aryantripathi16022006@gmail.com>
2025-03-08 09:09:33 +0530

    Time: 3 ms (6.58%) | Memory: 8.3 MB (62.68%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -8,7 +8,7 @@ public:
         for (int i = 0; i <= n - k; i++) {  // Ensure we check all k-length substrings
             c = 0;  // Reset count for each new window
 
-            for (int j = i; j < i + k; j++) {  // Correct window iteration
+            for (int j = i; j-i+1<= k; j++) { 
                 if (blocks[j] == 'W') {
                     c++;  // Count 'W' blocks in the window
                 }
