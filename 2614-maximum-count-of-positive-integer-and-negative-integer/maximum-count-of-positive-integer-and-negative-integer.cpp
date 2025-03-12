715da0dcf55ca51d9c0f3edea4051831728de825
aryan3665 <aryantripathi16022006@gmail.com>
2025-03-12 09:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (95.51%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -3,15 +3,11 @@ public:
     int maximumCount(vector<int>& nums) {
 
 
-        auto lambdap=[](int num){
-            return num>0;
-        };
-         auto lambdan=[](int num){
-            return num<0;
-        };
-       int countpo=count_if(begin(nums),end(nums),lambdap);
-        int countneg=count_if(begin(nums),end(nums),lambdan);
-        return max(countpo,countneg);
-
+     int posIndex = lower_bound(nums.begin(), nums.end(), 1) - nums.begin();
+      int posCount = nums.size() - posIndex;
+      int firstzero=lower_bound(nums.begin(),nums.end(),0)-nums.begin();
+      int neg=firstzero;
+      return max(posCount,neg);
+    
     }
 };
