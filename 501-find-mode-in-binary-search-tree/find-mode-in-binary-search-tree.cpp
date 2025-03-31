14c5cd811d8c5c98d4a85e19638f9e2c32d5ac2b
aryan3665 <aryantripathi16022006@gmail.com>
2025-03-31 12:25:43 +0530

    Time: 5 ms (30.70%) | Memory: 25.9 MB (18.05%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void traverse(TreeNode* root, map<int, int>& freq) {
+        if (!root) return;
+        freq[root->val]++;
+        traverse(root->left, freq);
+        traverse(root->right, freq);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        map<int, int> freq;
+        traverse(root, freq);
+
+        int maxFreq = 0;
+        for (auto it : freq) {
+            maxFreq = max(maxFreq, it.second);
+        }
+
+        vector<int> ans;
+        for (auto it : freq) {
+            if (it.second == maxFreq) {
+                ans.push_back(it.first);
+            }
+        }
+
+        return ans;
+    }
+};
