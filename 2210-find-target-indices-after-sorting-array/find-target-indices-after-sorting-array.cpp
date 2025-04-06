8a08f98a0c663d3eedad4c03d1751fae61e67556
aryan3665 <aryantripathi16022006@gmail.com>
2025-04-06 09:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (99.00%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+         for (int i = 1; i < nums.size(); i++) {
+        int key = nums[i]; // current element
+        int j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            j--;
+        }
+
+        nums[j + 1] = key; // insert key at correct position
+    }
+       
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target){
