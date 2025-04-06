f80485f3ef76e10564434d384db7b8a76dee704b
aryan3665 <aryantripathi16022006@gmail.com>
2025-04-06 09:30:15 +0530

    Time: 11 ms (33.48%) | Memory: 86 MB (99.13%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -1,27 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-void inorder(TreeNode*root,vector<int>&v){
-    if(!root)return;
-    inorder(root->left,v);
-    v.push_back(root->val);
-    inorder(root->right,v);
-}
+    void pushLeft(TreeNode* root, stack<TreeNode*>& st) {
+        while (root) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>v;
-        inorder(root1,v);
-        inorder (root2,v);
-         sort(v.begin(),v.end());
-         return v;
+        stack<TreeNode*> s1, s2;
+        vector<int> result;
+
+        pushLeft(root1, s1);
+        pushLeft(root2, s2);
+
+        while (!s1.empty() || !s2.empty()) {
+            stack<TreeNode*>& st = 
+                s2.empty() || (!s1.empty() && s1.top()->val <= s2.top()->val) ? s1 : s2;
+
+            TreeNode* node = st.top(); st.pop();
+            result.push_back(node->val);
+            pushLeft(node->right, st);
+        }
+
+        return result;
     }
 };
