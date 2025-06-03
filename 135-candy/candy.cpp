69b98b95fa74107dab07a6d765723f081de7e046
aryan3665 <aryantripathi16022006@gmail.com>
2025-06-03 08:00:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (70.54%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,31 +2,28 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int candy = n, i=1;
-        while(i<n){
-            if(ratings[i] == ratings[i-1]){
-                i++;
-                continue;
+        vector<int> ans(n, 1);
+
+        // Left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                ans[i] = ans[i - 1] + 1;
             }
-            
-            //For increasing slope
-            int peak = 0;
-            while(ratings[i] > ratings [i-1]){
-                peak++;
-                candy += peak;
-                i++;
-                if(i == n) return candy;
-            }
-            
-            //For decreasing slope
-            int valley = 0;
-            while(i<n && ratings[i] < ratings[i-1]){
-                valley++;
-                candy += valley;
-                i++;
-            }
-            candy -= min(peak, valley); //Keep only the higher peak
         }
-        return candy;
+
+        // Right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                ans[i] = max(ans[i], ans[i + 1] + 1);
+            }
+        }
+
+        // Sum up the candies
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += ans[i];
+        }
+
+        return sum;
     }
 };
