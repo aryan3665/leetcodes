024d98aa36d2c2650e2c68e1b90c725493e9e73d
aryan3665 <aryantripathi16022006@gmail.com>
2025-07-23 08:58:09 +0530

    Time: 681 ms (7.87%) | Memory: 425 MB (5.24%) - LeetSync

---
+++ 869-similar-string-groups/similar-string-groups.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool similar(string& s, string& t) {
+        if (s.size() != t.size()) return false;
+        vector<int> diff;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != t[i]) diff.push_back(i);
+        }
+        if (diff.size() == 0) return true; // same strings
+        if (diff.size() != 2) return false;
+
+        return s[diff[0]] == t[diff[1]] && s[diff[1]] == t[diff[0]];
+    }
+
+    void dfs(int node, unordered_map<int, vector<int>>& adj, vector<bool>& visited) {
+        visited[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+
+    int numSimilarGroups(vector<string>& strs) {
+        unordered_map<int, vector<int>> adj;
+        int n = strs.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (similar(strs[i], strs[j])) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+
+        vector<bool> visited(n, false);
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+};
