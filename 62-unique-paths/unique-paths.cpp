5548e0da8998e36565ffca6906a5395394b505a9
aryan3665 <aryantripathi16022006@gmail.com>
2025-07-26 12:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (72.41%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int dp[100][100];
-
-    int solve(int i, int j) {
-        if (i == 0 && j == 0) return 1;
-        if (i < 0 || j < 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = solve(i - 1, j) + solve(i, j - 1);
-    }
-
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp)); // Correct spelling of memset
-        return solve(m - 1, n - 1);  
+        int dp[100][100];
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 1;  // Only one path along top row or left column
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
