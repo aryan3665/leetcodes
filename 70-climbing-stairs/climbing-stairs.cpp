05bdb5ac374d53a64d6b4e43bc287199e09ac11c
aryan3665 <aryantripathi16022006@gmail.com>
2025-07-26 13:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (73.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+int dp[46];
+int fun (int n){
+    if(n<0)return 0;
+  if(n==0)return 1;
+  if(dp[n]!=-1)return dp[n];
+int one=fun(n-1);
+int two;
+
+two=fun(n-2);
+  return dp[n]= (one+two);
+}
     int climbStairs(int n) {
-        // Base cases
-        if (n <= 1) return 1;
-
-        // Two variables to store the last two results
-        int n1 = 1; // ways to reach step 0
-        int n2 = 1; // ways to reach step 1
-        int n3 = 0;
-
-        // Bottom-up DP from step 2 to n
-        for (int i = 2; i <= n; i++) {
-            n3 = n1 + n2; // current step ways = previous two steps
-            n1 = n2;
-            n2 = n3;
-        }
-
-        return n3;
+        memset(dp,-1,sizeof(dp));
+        return fun(n);
     }
 };
