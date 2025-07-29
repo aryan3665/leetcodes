3d019a5180847af29c167280da7dce9bf0cceb7c
aryan3665 <aryantripathi16022006@gmail.com>
2025-07-29 09:29:02 +0530

    Time: 3 ms (56.05%) | Memory: 18 MB (28.51%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-set<vector<int>>s;
-void solve(vector<int>& arr,int idx,vector<int>&combs,vector<vector<int>> &ans,int target){
+void solve(int i,vector<int>& candidates,int target,vector<int>&temp, vector<vector<int>>&ans){
 
-
-if(idx==arr.size()||target<0)return;
-if(target==0){
-    if(s.find(combs)==s.end()){
-
-    ans.push_back(combs);
-    s.insert(combs);
-    }
+  if(target==0){
+    ans.push_back(temp);
     return;
-}
+  }
+  if(i==candidates.size()||target<0){
+    return;
+  }
 
-    combs.push_back(arr[idx]);
-    solve(arr,idx+1,combs,ans,target-arr[idx]);
-    solve(arr,idx,combs,ans,target-arr[idx]);
-    combs.pop_back();
-    solve(arr,idx+1,combs,ans,target);
+
+    //not take
+
+    solve(i+1,candidates,target,temp,ans);
+    //taken
+    temp.push_back(candidates[i]);
+    solve(i,candidates,target-=candidates[i],temp,ans);
+    target+=candidates[i];
+    temp.pop_back();
 }
-    vector<vector<int>> combinationSum(vector<int>&arr, int target) {
-        vector<int>combs;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-        
-        solve(arr,0,combs,ans,target);
+        vector<int>temp;
+        solve(0,candidates,target,temp,ans);
         return ans;
     }
 };
