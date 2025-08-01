77ac5119d78b7a935446361d3628c3ca5e79a117
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-01 11:27:11 +0530

    Time: 149 ms (36.65%) | Memory: 17.5 MB (59.47%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,37 +1,39 @@
-#include <vector>
-#include <cstring> 
-
-using namespace std;
-
 class Solution {
-private:
-    int sumOf(const vector<int>& nums) {
-        int sum = 0;
-        for (int num : nums)
-            sum += num;
-        return sum;
-    }
-
-    bool rec(int i, const vector<int>& nums, int n, int sum, vector<vector<int>>& dp) {
-        if (sum == 0) return true;
-        if (sum < 0 || i >= n) return false;
-        if (dp[i][sum] != -1) return dp[i][sum] == 1;
-
-        bool dont = rec(i + 1, nums, n, sum, dp);
-        bool pick = (nums[i] <= sum) ? rec(i + 1, nums, n, sum - nums[i], dp) : false;
-        
-        dp[i][sum] = (pick || dont) ? 1 : 0;
-        return pick || dont;
-    }
-
 public:
     bool canPartition(vector<int>& nums) {
+        int sum = 0;
         int n = nums.size();
-        int sum = sumOf(nums);
-        if (sum % 2 == 1) return false;
-        sum /= 2;
-        
-        vector<vector<int>> dp(n, vector<int>(sum + 1, -1));
-        return rec(0, nums, n, sum, dp);
+
+        for (auto x : nums) {
+            sum += x;
+        }
+
+        // If sum is odd, it can't be partitioned into two equal subsets
+        if (sum % 2 != 0) return false;
+
+        int s1 = sum / 2;
+        vector<vector<bool>> dp(n, vector<bool>(s1 + 1, false));
+
+        // Base case: sum = 0 is always possible
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+        // Base case: first element
+        if (nums[0] <= s1)
+            dp[0][nums[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= s1; j++) {
+                bool nt = dp[i - 1][j]; // not take
+                bool t = false;
+                if (j >= nums[i]) {
+                    t = dp[i - 1][j - nums[i]]; // take
+                }
+                dp[i][j] = nt || t;
+            }
+        }
+
+        return dp[n - 1][s1];
     }
 };
