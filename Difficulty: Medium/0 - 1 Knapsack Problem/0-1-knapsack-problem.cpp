fe49054be433dc7c4ffe6f5af960652d1f641b05
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-04 10:01:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,24 +1,25 @@
 class Solution {
   public:
-    int solve(vector<int> &wt, vector<int> &val, int w, int n, vector<vector<int>> &dp) {
-        if (n == 0 || w == 0) return 0;
+  int solve(int W,vector<int>&val,vector<int>&wt,int index,vector<vector<int>>&dp){
+    if(index == 0){
+    if(wt[0] <= W) return val[0];
+    else return 0;
+}
 
-        if (dp[n][w] != -1) return dp[n][w];
 
-        if (wt[n - 1] <= w) {
-            return dp[n][w] = max(
-                val[n - 1] + solve(wt, val, w - wt[n - 1], n - 1, dp),
-                solve(wt, val, w, n - 1, dp)
-            );
-        } else {
-            return dp[n][w] = solve(wt, val, w, n - 1, dp);
-        }
-    }
-
+if(dp[index][W]!=-1)return dp[index][W];
+      int notpick=solve(W,val,wt,index-1,dp);
+      int pick=INT_MIN;
+      if(W>=wt[index]){
+          pick=val[index]+solve(W-wt[index],val,wt,index-1,dp);
+      }
+      return dp[index][W]=max(pick,notpick);
+  }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        int n = val.size();
-        // Create a 2D DP table since both 'n' and 'W' vary
-        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
-        return solve(wt, val, W, n, dp);
+        // code here
+        int n=wt.size();
+        vector<vector<int>>dp(n,vector<int>(W+1,-1));
+        
+        return solve(W,val,wt,n-1,dp);
     }
 };
