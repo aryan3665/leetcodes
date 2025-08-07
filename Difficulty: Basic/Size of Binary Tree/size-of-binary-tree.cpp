a7b5b7833a13e8abe1fb0b55d7cbb61bcd550809
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-07 10:45:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.cpp
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.cpp
@@ -30,14 +30,14 @@ class Solution {
         while (!q.empty()) {
             int n = q.size(); // Number of nodes at current level
             ans+=n;
-            vector<int> temp; // To store current level values
+     
 
             // Process all nodes of the current level
             while(n--) {
                 Node* fronty = q.front(); // Get the front node
                 q.pop(); // Remove it from the queue
 
-                temp.push_back(fronty->data); // Store its data
+              
 
                 // Push left child if exists
                 if (fronty->left)
