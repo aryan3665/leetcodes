283bb8d072c8b6a44b858acfdbfd8007e1cc4249
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-07 10:50:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
@@ -15,41 +15,12 @@ struct Node
 // Function to return a list containing the level order traversal in spiral form.
 class Solution {
   public:
+  int Sumt(Node*root){
+       if(!root)return 0;
+        return root->data+Sumt(root->left)+Sumt(root->right);
+  }
     int sumBT(Node* root) {
         // code here
-      int sum=0;
-
-    
-
-        // Queue for level-order traversal
-        queue<Node*> q;
-        q.push(root);
-
-        // Traverse level by level
-        while (!q.empty()) {
-            int n = q.size(); // Number of nodes at current level
-            vector<int> temp; // To store current level values
-
-            // Process all nodes of the current level
-            while(n--) {
-                Node* fronty = q.front(); // Get the front node
-                sum+=fronty->data;
-                q.pop(); // Remove it from the queue
-
-               
-                // Push left child if exists
-                if (fronty->left)
-                    q.push(fronty->left);
-
-                // Push right child if exists
-                if (fronty->right)
-                    q.push(fronty->right);
-            }
-
-         
-    
-        }
-
-        return sum; 
+       return Sumt(root);
     }
 };
