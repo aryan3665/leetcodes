a0a897d23e24b3f63175bdb83aef43e714febef1
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-09 12:10:23 +0530

    Time: 15 ms (91.45%) | Memory: 12.8 MB (86.96%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(string& text1, string& text2, int index1, int index2, vector<vector<int>>& dp){
-        if(index1 < 0 || index2 < 0) return 0;
-        if(dp[index1][index2] != -1) return dp[index1][index2];
+    // int solve(string& text1, string& text2, int index1, int index2, vector<vector<int>>& dp){
+    //     if(index1 < 0 || index2 < 0) return 0;
+    //     if(dp[index1][index2] != -1) return dp[index1][index2];
 
-        // If characters match
-        if(text1[index1] == text2[index2]){
-            return dp[index1][index2] = 1 + solve(text1, text2, index1-1, index2-1, dp);
-        }
-        // If characters don't match
-        return dp[index1][index2] = max(solve(text1, text2, index1-1, index2, dp), 
-                                      solve(text1, text2, index1, index2-1, dp));
-    }
+    //     // If characters match
+    //     if(text1[index1] == text2[index2]){
+    //         return dp[index1][index2] = 1 + solve(text1, text2, index1-1, index2-1, dp);
+    //     }
+    //     // If characters don't match
+    //     return dp[index1][index2] = max(solve(text1, text2, index1-1, index2, dp), 
+    //                                   solve(text1, text2, index1, index2-1, dp));
+    // }
     
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
