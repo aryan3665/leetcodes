ee1ee6f6100fbbed3528d3924408690a5c8a6d9e
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-11 09:39:32 +0530

    Time: 47 ms (90.23%) | Memory: 13.3 MB (81.29%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string &s1, string &s2, int index1, int index2) {
-        // base case
-        if(index1 < 0 || index2 < 0) return 0;
-        if(dp[index1][index2] != -1) return dp[index1][index2];
+int dp[1001][1001];
+int solve(string &s1,string &s2,int index1,int index2){
+    //base case
+    if(index1<0||index2<0)return 0;
+    if(dp[index1][index2]!=-1)return dp[index1][index2];
 
-        // if the char matches
-        if(s1[index1] == s2[index2]) {
-            return dp[index1][index2] = 1 + solve(s1, s2, index1-1, index2-1);
-        }
-        // if char not match
-        return dp[index1][index2] = max(solve(s1, s2, index1-1, index2), solve(s1, s2, index1, index2-1));
+    //if the char matches
+    if(s1[index1]==s2[index2]){
+        return dp[index1][index2]=1+solve(s1,s2,index1-1,index2-1);
     }
-    
+    //if char not match
+    return dp[index1][index2]=max(solve(s1,s2,index1-1,index2),solve(s1,s2,index1,index2-1));
+}
     int longestPalindromeSubseq(string s) {
-        string s1 = s;
-        reverse(s.begin(), s.end());
-        string s2 = s;
-        int n = s1.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(s1, s2, n-1, n-1);
+        string s1=s;
+        reverse(s.begin(),s.end());
+        string s2=s;
+        int n=s1.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(s1,s2,n-1,n-1);
+        
     }
 };
