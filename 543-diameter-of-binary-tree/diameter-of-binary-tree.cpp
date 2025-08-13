c54e8346830d2f619608ee1e2f9f6fb6ff9736e1
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-13 13:26:43 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (42.44%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,15 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int diameterOfBinaryTree(TreeNode* root) {
-        int dia=0;
-        treedepth(root, dia);
-        return dia;
+    int height(TreeNode* root , int &diameter){ 
+        /*here we use int &diameter becuase updates to diameter are lost
+         after returning from the recursion. We need to pass diameter
+         by reference so that all recursive calls share the same variable*/
+        if(root == NULL) return 0;
+        int leftheight = height(root->left,diameter);
+        int rightheight = height(root->right,diameter);
+        diameter = max(diameter , (leftheight + rightheight));
+        return 1 + max(leftheight,rightheight);
     }
-    int treedepth(TreeNode* root, int& dia){
-        if(root==NULL) return 0;
-        int countL = treedepth(root->left, dia);
-        int countR = treedepth(root->right, dia);
-        dia = max(dia, countL+countR);
-        return max(countL, countR) + 1;
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        height(root,diameter);
+        return diameter;
     }
 };
