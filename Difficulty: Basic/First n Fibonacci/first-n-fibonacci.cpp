0ccc035304fb6e24b520b2435588410280d4e6c7
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-13 10:31:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.cpp
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.cpp
@@ -5,16 +5,16 @@ class Solution {
     // Function to return list containing first n fibonacci numbers.
     vector<int> fibonacciNumbers(int n) {
         // code here
-        vector<int>dp(n+1);
+        vector<int>dp(n);
         vector<int>ans;
         dp[0]=0;
         dp[1]=1;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
        for(int i=0;i<n;i++){
            ans.push_back(dp[i]);
        }
-       return ans;
+       return dp;
     }
 };
