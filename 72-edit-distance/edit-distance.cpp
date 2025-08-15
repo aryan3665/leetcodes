76ea272e668074be32e4df8fb8173e5e87ee8d82
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-15 09:29:59 +0530

    Time: 3 ms (90.18%) | Memory: 13.1 MB (47.07%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-int dp[501][501];
-int solve(string &word1, string &word2,int i,int j){
-    if(i<0)return j+1;
-    if(j<0)return i+1;
-    if(dp[i][j]!=-1)return dp[i][j];
-    if(word1[i]==word2[j])return solve(word1,word2,i-1,j-1);
-    else{
-        //if the string char not matching then we are trying out all the  ways to perform the operation and taking out the minimum of all the operations
-        int inserted=1+solve(word1,word2,i,j-1);
-        int deleted=1+solve(word1,word2,i-1,j);
-        int replace=1+solve(word1,word2,i-1,j-1);
-        return dp[i][j]=min(inserted,min(deleted,replace));
-    }
-}
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
-        int n=word1.size();
-        int m=word2.size();
-        return solve(word1,word2,n-1,m-1);
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        // Base cases
+        for(int i = 0; i <= n; i++) dp[i][0] = i;
+        for(int j = 0; j <= m; j++) dp[0][j] = j;
+        
+        // Fill DP table
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+                }
+            }
+        }
+        
+        return dp[n][m];
     }
 };
