d2cd58c250cacdbad2d6a88843b1504a39d03fda
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-15 09:16:58 +0530

    Time: 45 ms (16.69%) | Memory: 10.3 MB (94.77%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int dp[501][501];
+int solve(string &word1, string &word2,int i,int j){
+    if(i<0)return j+1;
+    if(j<0)return i+1;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(word1[i]==word2[j])return solve(word1,word2,i-1,j-1);
+    else{
+        int inserted=1+solve(word1,word2,i,j-1);
+        int deleted=1+solve(word1,word2,i-1,j);
+        int replace=1+solve(word1,word2,i-1,j-1);
+        return dp[i][j]=min(inserted,min(deleted,replace));
+    }
+}
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof(dp));
+        int n=word1.size();
+        int m=word2.size();
+        return solve(word1,word2,n-1,m-1);
+    }
+};
