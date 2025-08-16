baf366071c74e8accf994af460012d7e8e3dc131
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-16 10:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (42.23%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int i, bool canBuy, vector<vector<int>>& dp) {
-        if (i == prices.size()) return 0;
-
-        if (dp[i][canBuy] != -1) return dp[i][canBuy];
-
-        int profit = 0;
-        if (canBuy) {
-            // Either buy today or skip
-            profit = max(-prices[i] + solve(prices, i+1, false, dp),
-                         solve(prices, i+1, true, dp));
-        } else {
-            // Either sell today or skip
-            profit = max(prices[i] + solve(prices, i+1, true, dp),
-                         solve(prices, i+1, false, dp));
-        }
-
-        return dp[i][canBuy] = profit;
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return solve(prices, 0, true, dp); // start with "canBuy" = true
+        int dp[n + 1][2];
+
+        // Base case: when day = n (no days left)
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 1) {
+                    // Either buy today or skip
+                    profit = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } else {
+                    // Either sell today or skip
+                    profit = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+                dp[i][buy] = profit; 
+            }
+        }
+
+        return dp[0][1];
     }
 };
