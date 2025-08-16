78463306ada3f93a1685eb22504263cdb59dc23e
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-16 11:55:20 +0530

    Time: 123 ms (65.74%) | Memory: 16.7 MB (75.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int sum) {
-        int n = nums.size();
-        bool dp[n][sum + 1];
-
-        // Initialize all values to false
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                dp[i][j] = false;
-            }
-        }
-
-        // Base case: sum 0 is always possible (empty subset)
-        for (int i = 0; i < n; i++) {
-            dp[i][0] = true;
-        }
-
-        // Base case: first element can make sum nums[0] if it's <= sum
-        if (nums[0] <= sum) {
-            dp[0][nums[0]] = true;
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                bool notPick = dp[i - 1][j];
-                bool pick = false;
-                if (j >= nums[i]) {
-                    pick = dp[i - 1][j - nums[i]];
-                }
-                dp[i][j] = notPick || pick;
-            }
-        }
-
-        return dp[n - 1][sum];
+  bool solve(vector<int>& arr, int sum) {
+    int n = arr.size();
+    vector<bool> prev(sum + 1, false);
+    
+    // Base case: sum 0 is always possible
+    prev[0] = true;
+    
+    // Base case: first element
+    if (arr[0] <= sum) {
+        prev[arr[0]] = true;
     }
+    
+    for (int i = 1; i < n; i++) {
+        vector<bool> curr(sum + 1, false);
+        curr[0] = true; // base case
+        
+        for (int j = 1; j <= sum; j++) {
+            bool notPick = prev[j];
+            bool pick = false;
+            if (j >= arr[i]) {
+                pick = prev[j - arr[i]];
+            }
+            curr[j] = notPick || pick;
+        }
+        prev = curr;
+    }
+    
+    return prev[sum];
+}
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for (auto it : nums) {
