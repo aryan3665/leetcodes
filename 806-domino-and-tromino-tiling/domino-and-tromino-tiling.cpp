57ac00039a7ca3205c9c45e502d754d39825a328
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-16 08:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (76.81%) - LeetSync

---
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+int mod=1e9+7;
+    int numTilings(int n) {
+        int dp[1001];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i=4;i<=n;i++){
+            dp[i]=(2*dp[i-1]%mod+dp[i-3]%mod)%mod;
+        }
+        return dp[n];
+    }
+};
