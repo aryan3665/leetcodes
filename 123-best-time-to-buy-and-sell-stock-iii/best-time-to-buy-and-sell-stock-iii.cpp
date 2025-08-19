4f2e7cc0f1c6ddf6b89ac321c0c48bebf4c97c00
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-19 08:14:14 +0530

    Time: 470 ms (12.67%) | Memory: 223.9 MB (24.68%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-   int maxProfit(vector<int>& prices) {
-        if(!prices.size())
-            return 0;
-        int n = prices.size();
-        int left[n], right[n];
-        int leftmin = prices[0], rightmax = prices[n-1], maxprofit = 0;
-        left[0] = 0; right[n - 1] = 0;                                     // because we can't make any profit with just 1 element
-        int i,j;
-        for(i = 1, j = n-2; i < n, j >= 0; i++, j--){
-            leftmin  = min(leftmin, prices[i]);                            // find the minimum price till now
-            left[i]  = max(left[i - 1], prices[i] - leftmin);              // max of selling today or skipping (previous max profit)
-            rightmax = max(rightmax, prices[j]);                           // find the maximum price to the right
-            right[j] = max(right[j + 1], rightmax - prices[j]);            // max of buying today or skipping
+    int solve(int index, int buy, int maxt, vector<vector<vector<int>>>& dp, vector<int>& prices) {
+        if (maxt == 0) return 0;
+        if (index == prices.size()) return 0;
+        if (dp[index][buy][maxt] != -1) return dp[index][buy][maxt];
+
+        if (buy) {
+            return dp[index][buy][maxt] = max(
+                -prices[index] + solve(index + 1, 0, maxt, dp, prices),
+                solve(index + 1, 1, maxt, dp, prices)
+            );
+        } else {
+            return dp[index][buy][maxt] = max(
+                prices[index] + solve(index + 1, 1, maxt - 1, dp, prices),
+                solve(index + 1, 0, maxt, dp, prices)
+            );
         }
-        for(int i = 0; i < n; i++){
-            maxprofit = max(maxprofit, left[i] + right[i]);
-        }
-        return maxprofit;
     }
-    
-    // EG: PRICES = [3,3,5,0,0,3,1,4]
-    //     LEFT   = [0,0,2,2,2,2,2,3]
-    //     RIGHT  = [2,2,4,4,4,1,0,0]
-    //     SUM    = [2,2,6,6,6,3,2,3] 
-	//     MAX PROFIT = 6
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        // dp[index][buy][transactionsLeft]
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, -1)));
+        return solve(0, 1, 2, dp, prices);
+    }
 };
