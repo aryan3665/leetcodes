6bb7509648c0457a5ff406095329600c110249da
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-24 07:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 60 MB (59.08%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<int> ans;
-        int count = 0;
+        int n=nums.size();
+        int i=0;
+        //ek window size me 1 se jada zero nhi ana chahiye
+        int zero=0;
+        int maxi=INT_MIN;
+        for(int j=0;j<n;j++){
+            if(nums[j]==0)zero++;
 
-        // Step 1: Break array into blocks of 1's separated by 0's
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                count++;
-            } else {
-                ans.push_back(count);
-                count = 0;
+            while(zero>1){
+                if(nums[i]==0)zero--;
+                i++;
             }
+
+            maxi=max(maxi,j-i);
         }
-        ans.push_back(count); // push last segment of 1's
-
-        // Step 2: compute max by merging two consecutive segments
-        int maxsum = 0;
-        for (int i = 1; i < ans.size(); i++) {
-            maxsum = max(maxsum, ans[i] + ans[i - 1]);
-        }
-
-        // Step 3: handle edge case where array is all 1's
-        if (maxsum == 0) maxsum = *max_element(ans.begin(), ans.end());
-        if (maxsum == nums.size()) maxsum--; 
-
-        return maxsum;
+        return maxi;
     }
 };
