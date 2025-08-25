ca236bf9ea2b113b4a18812b95b0a502b3ce17f8
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-25 08:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (53.47%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    vector<int> dp;
-
-    int solve(int i, vector<int>& days, vector<int>& costs) {
-        if (i >= days.size()) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        // 1-day pass
-        int cost1 = costs[0] + solve(i + 1, days, costs);
-
-        // 7-day pass
-        int j = i;
-        while (j < days.size() && days[j] < days[i] + 7) {
-            j++;
-        }
-        int cost7 = costs[1] + solve(j, days, costs);
-
-        // 30-day pass
-        j = i;
-        while (j < days.size() && days[j] < days[i] + 30) {
-            j++;
-        }
-        int cost30 = costs[2] + solve(j, days, costs);
-
-        return dp[i] = min({cost1, cost7, cost30});
-    }
+    
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        dp.assign(days.size(), -1);
-        return solve(0, days, costs);
+        int n = days.size();
+        vector<int> dp(n + 1, 0);  // dp[i] = min cost to cover travel from i to end
+        dp[n] = 0;  // base case: no days left → cost 0
+
+        for (int i = n - 1; i >= 0; i--) {
+            // 1-day pass
+            int cost1 = costs[0] + dp[i + 1];
+
+            // 7-day pass
+            int j = i;
+            while (j < n && days[j] < days[i] + 7) {
+                j++;
+            }
+            int cost7 = costs[1] + dp[j];
+
+            // 30-day pass
+            j = i;
+            while (j < n && days[j] < days[i] + 30) {
+                j++;
+            }
+            int cost30 = costs[2] + dp[j];
+
+            dp[i] = min({cost1, cost7, cost30});
+        }
+
+        return dp[0];  // min cost starting from first travel day
     }
 };
+
