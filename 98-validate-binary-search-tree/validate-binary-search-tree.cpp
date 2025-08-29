04479cdab9032d3b4929a6e235079b67b48244f5
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-29 09:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (75.19%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,24 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool validate(TreeNode* node, long long minValue, long long maxValue){
-        if (!node) return true;  
-        if (node->val <= minValue || node->val >= maxValue) return false;  
-        return validate(node->left, minValue, node->val) && 
-               validate(node->right, node->val, maxValue);
+    bool solve(TreeNode* root, long long &prev) {
+        if (!root) return true;
+
+        if (!solve(root->left, prev)) return false;
+
+        if (root->val <= prev) return false;
+        prev = root->val;
+
+        return solve(root->right, prev);
     }
 
     bool isValidBST(TreeNode* root) {
-        return validate(root, LLONG_MIN, LLONG_MAX);
+        long long prev = LLONG_MIN; // use long long to handle INT_MIN case
+        return solve(root, prev);
     }
 };
