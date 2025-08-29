965e9dd2c8a32e49f1d7afeba09c1ab958eccdd2
aryan3665 <aryantripathi16022006@gmail.com>
2025-08-29 09:37:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.cpp
+++ Difficulty: Easy/Check for BST/check-for-bst.cpp
@@ -1,147 +1,33 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-#define MAX_HEIGHT 100000
-
-// Tree Node
-struct Node {
+/*
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
 
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = right = nullptr;
     }
 };
-
-
-// } Driver Code Ends
+*/
 
 class Solution {
   public:
-  bool bst(Node*root,int&prev){
+  bool s(Node*root,long long &prev){
       if(!root)return 1;
       
       
-      
-      
-      bool l=bst(root->left,prev);
-      if(l==0)return 0;
-      
-      if(root->data<=prev)return 0;
+      if(!s(root->left,prev))return false;
+      if(root->data<prev)return false;
       prev=root->data;
-     return bst(root->right,prev);
+      return s(root->right,prev);
   }
     // Function to check whether a Binary Tree is BST or not.
     bool isBST(Node* root) {
         // Your code here
-        int prev=INT_MIN;
-        return bst(root,prev);
-        
+        long long prev=INT_MIN;
+        return s(root,prev);
         
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-void inorder(Node* root, vector<int>& v) {
-    if (root == NULL)
-        return;
-
-    inorder(root->left, v);
-    v.push_back(root->data);
-    inorder(root->right, v);
-}
-
-int main() {
-
-    int t;
-    string tc;
-    getline(cin, tc);
-    t = stoi(tc);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        // if (s[0] == '9') {
-        //     cout << "false\n";
-        //     return 0;
-        // }
-        Node* root = buildTree(s);
-        Solution ob;
-        if (ob.isBST(root))
-            cout << "true\n";
-
-        else
-            cout << "false\n";
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
