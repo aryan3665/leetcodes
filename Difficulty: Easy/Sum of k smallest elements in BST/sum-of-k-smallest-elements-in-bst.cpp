25b25afb45e93f7a86e8df1966ff075eab259eba
aryan3665 <aryantripathi16022006@gmail.com>
2025-09-02 10:05:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum of k smallest elements in BST/sum-of-k-smallest-elements-in-bst.cpp
+++ Difficulty: Easy/Sum of k smallest elements in BST/sum-of-k-smallest-elements-in-bst.cpp
@@ -1,119 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-	int data;
-	struct Node *left;
-	struct Node *right;
-};
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-	Node* temp = new Node;
-	temp->data = val;
-	temp->left = NULL;
-	temp->right = NULL;
-
-	return temp;
-}
-// Function to Build Tree
-Node* buildTree(string str)
-{
-	// Corner Case
-	if (str.length() == 0 || str[0] == 'N')
-		return NULL;
-
-	// Creating vector of strings from input
-	// string after spliting by space
-	vector<string> ip;
-
-	istringstream iss(str);
-	for (string str; iss >> str; )
-		ip.push_back(str);
-
-	// Create the root of the tree
-	Node* root = newNode(stoi(ip[0]));
-
-	// Push the root to the queue
-	queue<Node*> queue;
-	queue.push(root);
-
-	// Starting from the second element
-	int i = 1;
-	while (!queue.empty() && i < ip.size()) {
-
-		// Get and remove the front of the queue
-		Node* currNode = queue.front();
-		queue.pop();
-
-		// Get the current node's value from the string
-		string currVal = ip[i];
-
-		// If the left child is not null
-		if (currVal != "N") {
-
-			// Create the left child for the current node
-			currNode->left = newNode(stoi(currVal));
-
-			// Push it to the queue
-			queue.push(currNode->left);
-		}
-
-		// For the right child
-		i++;
-		if (i >= ip.size())
-			break;
-		currVal = ip[i];
-
-		// If the right child is not null
-		if (currVal != "N") {
-
-			// Create the right child for the current node
-			currNode->right = newNode(stoi(currVal));
-
-			// Push it to the queue
-			queue.push(currNode->right);
-		}
-		i++;
-	}
-
-	return root;
-}
-
-int sum(Node* root, int k);
-
-
-
-int main()
-{
-
-	int t;
-	scanf("%d ", &t);
-	while (t--)
-	{
-		string s;
-		getline(cin >> ws, s);
-		int k;
-		cin >> k;
-		Node* root = buildTree(s);
-		cout << sum(root, k) << endl;
-	
-	
-cout << "~" << "\n";
-}
-	return 1;
-}
-
-
-
-
-
-
-// } Driver Code Ends
 
 
 //User function Template for C++
@@ -134,22 +18,26 @@ struct Node {
 
 // Function to find ceil of a given input in BST. If input is more 
 // than the max key in BST, return -1 
- void inorder(Node* root, vector<int>& ans) {
-        if (!root) return;
-        inorder(root->left, ans);
-        ans.push_back(root->data);
-        inorder(root->right, ans);
-    }
+ void ksum(Node*root,int&sum,int&k){
+     if(!root)return ;
+     ksum(root->left,sum,k);
+     k--;
+     if(k>=0){
+         sum+=root->data;
+     }
+         if(k<=0){
+             return;
+         }
+         ksum(root->right,sum,k);
+     
+ }
 int sum(Node* root, int k) 
 { 
   
     // Your code here
-    vector<int> ans;
-    inorder(root, ans);
-    int s=0;
-    for(int i=0;i<k;i++){
-        s+=ans[i];
-    }
-    return s;
+    int sum=0;
+    
+    ksum(root,sum,k);
+    return sum;
     
 } 
