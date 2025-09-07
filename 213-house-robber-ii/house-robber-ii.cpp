27e98e105160c06dbbc0a228d4492b5714cacdba
aryan3665 <aryantripathi16022006@gmail.com>
2025-09-07 13:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (77.78%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+int dp[101];
+int rob1(vector<int>&nums,int i,int end){
+    if(i>end)return 0;
+    if(dp[i]!=-1)return dp[i];
+
+
+    int yes=nums[i]+rob1(nums,i+2,end);
+    int no=rob1(nums,i+1,end);
+    return dp[i]=max(yes,no);
+}
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        if (n == 2) return max(nums[0], nums[1]);
+        int n=nums.size();
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        //steal in oth index house
+        memset(dp,-1,sizeof(dp));
+        int case1=rob1(nums,0,n-2);
+        //skip oth index house
+         memset(dp,-1,sizeof(dp));
+        int case2=rob1(nums,1,n-1);
 
-        // ---- CASE 1: Rob from house 0 to n-2 (exclude last house) ----
-        vector<int> dp1(n, 0);
-        dp1[0] = nums[0];
-        dp1[1] = max(nums[0], nums[1]);
-        for (int i = 2; i < n - 1; i++) {
-            dp1[i] = max(dp1[i - 1], dp1[i - 2] + nums[i]);
-        }
-        int case1 = dp1[n - 2];
 
-        // ---- CASE 2: Rob from house 1 to n-1 (exclude first house) ----
-        vector<int> dp2(n, 0);
-        dp2[1] = nums[1];
-        for (int i = 2; i < n; i++) {
-            dp2[i] = max(dp2[i - 1], dp2[i - 2] + nums[i]);
-        }
-        int case2 = dp2[n - 1];
-
-        return max(case1, case2);
+        return max(case1,case2); 
     }
 };
