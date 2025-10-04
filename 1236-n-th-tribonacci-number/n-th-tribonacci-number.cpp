a0bda43148dedc8c7c0400677135b7e8c8a1f679
aryan3665 <aryantripathi16022006@gmail.com>
2025-10-04 12:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (97.64%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -14,13 +14,19 @@ public:
         // memset(dp,-1,sizeof(dp));
         // return solve(n);
 
-        int dp[38];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i <= 37; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        
+        int n0=0;
+        int n1= 1;
+        int n2= 1;
+        int n3;
+        if(n==0)return 0;
+        if(n==1||n==2)return 1;
+        for (int i = 3; i <=n; i++) {
+            n3 = n0+ n1 +n2;
+            n0=n1;
+            n1=n2;
+            n2=n3;
         }
-        return dp[n];
+        return n3;
     }
 };
