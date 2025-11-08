650225937a1ed3e3608ebdb93032966266418e16
aryan3665 <aryantripathi16022006@gmail.com>
2025-11-08 12:25:32 +0530

    Time: 8 ms (61.92%) | Memory: 26.2 MB (47.05%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  bool iscorrect(int val, vector<int>& arr, int k) {
+        int s = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            s += ceil((double)arr[i] / val); 
+        }
+        return (s <= k);
+    }
+    int smallestDivisor(vector<int>& arr, int k) {
+         int l = 1;
+        int maxi = *max_element(arr.begin(), arr.end());
+        int r = maxi;
+        int ans = -1;  
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (iscorrect(mid, arr, k)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
