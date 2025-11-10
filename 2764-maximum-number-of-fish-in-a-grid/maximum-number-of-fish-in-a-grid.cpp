2681c98d0d472be81d0a3bba09a02d5327a41cd4
aryan3665 <aryantripathi16022006@gmail.com>
2025-11-10 10:57:54 +0530

    Time: 5 ms (72.60%) | Memory: 94.1 MB (79.04%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
 int r,c;
+int ans=INT_MIN;
 int row[4]={-1,1,0,0};
 int col[4]={0,0,-1,1};
 bool issafe(int i,int j){
     return (i>=0&&i<r&&j>=0&&j<c);
 }
+void dfs(vector<vector<int>>& grid,int i,int j,int &tempscore){
+    tempscore+=grid[i][j];
+    grid[i][j]=0;
+    for(int k=0;k<4;k++){
+        int nx=i+row[k];
+        int ny=j+col[k];
+        if(issafe(nx,ny)&&grid[nx][ny]>0){
+            dfs(grid,nx,ny,tempscore);
+        }
+    }
+}
     int findMaxFish(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
+      
          r=grid.size();
         c=grid[0].size();
-        int ans=INT_MIN;
+       
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
+                   int tempscore=0;
                 if(grid[i][j]>0){
-                    q.push({i,j});
+                 
+                    dfs(grid,i,j,tempscore);
+                    ans=max(ans,tempscore);
+                  
                 }
-                 int tempscore=0;
-                while(!q.empty()){
-                   
-                    auto it=q.front();
-                    q.pop();
-                    int i_=it.first;
-                    int j_=it.second;
-                    tempscore+=grid[i_][j_];
-                    grid[i_][j_]=0;
-                    for(int k=0;k<4;k++){
-                        int nx=i_+row[k];
-                        int ny=j_+col[k];
-                        if(issafe(nx,ny)&&grid[nx][ny]>0){
-                            q.push({nx,ny});
-                        }
-
-                    }
-
-                }
-                ans=max(ans,tempscore);
             }
         }
-        return ans==INT_MAX?-1:ans;
+               
+            
+        return ans==INT_MIN?0:ans;
     }
 };
