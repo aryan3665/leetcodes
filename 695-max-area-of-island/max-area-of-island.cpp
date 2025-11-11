f4ab7835651c7c29a36595f4f5915d2c7731eff3
aryan3665 <aryantripathi16022006@gmail.com>
2025-11-11 09:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (91.31%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -4,36 +4,30 @@ public:
     int row[4] = {0, 0, -1, 1};
     int col[4] = {-1, 1, 0, 0};
     bool issafe(int i, int j) { return (i >= 0 && j >= 0 && i < r && j < c); }
+    void dfs(int i,int j,vector<vector<int>>&grid,int &tempcount){
+        grid[i][j]=0;
+        tempcount++;
+        for(int k=0;k<4;k++){
+            int nx=i+row[k];
+            int ny=j+col[k];
+            if(issafe(nx,ny)&&grid[nx][ny]==1){
+                dfs(nx,ny,grid,tempcount);
+            }
+        }
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         r = grid.size();
         c = grid[0].size();
         int result = 0;
-        queue<pair<int, int>> q;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if (grid[i][j] == 1) {
-                    q.push({i, j});
-                    grid[i][j] = 0;
-                }
                 int tempcount = 0;
-                while (!q.empty()) {
-                    auto it = q.front();
-                    int x = it.first;
-                    int y = it.second;
-                    q.pop();
-                    tempcount++;
+                if (grid[i][j] == 1) {
+                    dfs(i,j,grid,tempcount);
+                    result = max(result, tempcount);
 
-                    for (int k = 0; k < 4; k++) {
-                        int nx = x + row[k];
-                        int ny = y + col[k];
-                        if (issafe(nx, ny) && grid[nx][ny] == 1) {
-                            grid[nx][ny] = 0;
-
-                            q.push({nx, ny});
-                        }
-                    }
-                }
-                result = max(result, tempcount);
+                } 
+              
             }
         }
         return result;
