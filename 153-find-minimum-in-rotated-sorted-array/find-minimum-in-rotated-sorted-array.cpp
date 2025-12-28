e6904303937b56d05b1ba709f30f3e020673bed7
aryan3665 <aryantripathi16022006@gmail.com>
2025-12-28 11:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (47.29%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        //just pickup minimum from sorted half and eliminate that portion as now we will get answer in unsorted half only 
-        int low=0;
         int n=nums.size();
-        int high=n-1;
+        int start=0;
+        int end=n-1;
         int ans=INT_MAX;
-        while(low<=high){
-            //check which half is sorted
-            //left half sorted
-            int mid=low+(high-low)/2;
-            if(nums[low]<=nums[mid]){
-                ans=min(ans,nums[low]);
-                low=mid+1;
-                //just stored minimum and elemenate this half
-
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            //identify the sorted half
+            if(nums[start]<=nums[mid]){
+                //left half sorted
+                ans=min(ans,nums[start]);
+                start=mid+1;
             }
             else{
-                //right half sorted
                 ans=min(ans,nums[mid]);
-                high=mid-1;
-                //just take minimum elemnt and move left
+                end=mid-1;
             }
-
         }
         return ans;
     }
