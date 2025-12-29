0f08ba7803a0af8d6c816be34415d6b76133e978
aryan3665 <aryantripathi16022006@gmail.com>
2025-12-29 10:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.36%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int mySqrt(int n) {
-         if (n == 0 || n == 1) return n;
-
-        int ans = 1;  // initialize safely
-        int l = 1;
-        int r = n / 2;
-
-        while (l <= r) {
-            long long mid = l + (r - l) / 2; // use long long to avoid overflow
-            if (mid * mid <= n) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+bool ispossible(long long m,int x){
+    return m*m<=x;
+}
+    int mySqrt(int x) {
+        long long low=1;
+        long long high=x;
+        long long ans=0;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            if(ispossible(mid,x)){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return ans;
