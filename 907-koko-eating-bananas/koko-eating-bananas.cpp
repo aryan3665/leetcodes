20f05d4372dce6721245491ce4b26e6473b59d66
aryan3665 <aryantripathi16022006@gmail.com>
2025-12-29 10:27:11 +0530

    Time: 3 ms (95.00%) | Memory: 23 MB (43.66%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    bool isable(vector<int>& piles, int m, int h) {
-        long long k = 0;  // total hours (use long long to avoid overflow)
-        for (int i = 0; i < piles.size(); i++) {
-            // hours needed for pile[i] = ceil(piles[i] / m)
-            k += (piles[i] + m - 1) / m;
-            if (k > h) return false; 
-        }
-        return true;
+bool ispossible(int m,vector<int>&piles,int h){
+    long long total=0;
+    for(int i=0;i<piles.size();i++){
+        total+=(piles[i] + m - 1) / m;
     }
-
+    return total<=h;
+}
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1;
-        int r = *max_element(piles.begin(), piles.end());
-        int ans = r;  
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (isable(piles, mid, h)) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        int n=piles.size();
+        int l=1;
+        int maxi=*max_element(piles.begin(),piles.end());
+        int hi=maxi;
+        int ans=hi;
+        while(l<=hi){
+            int mid=l+(hi-l)/2;
+            if(ispossible(mid,piles,h)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else{
+                l=mid+1;
             }
         }
         return ans;
