f00983dbefee0df640984cb9d817440840334d7d
aryan3665 <aryantripathi16022006@gmail.com>
2025-12-30 21:28:41 +0530

    Time: 8 ms (58.99%) | Memory: 26.3 MB (14.72%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-  bool iscorrect(int val, vector<int>& arr, int k) {
-        int s = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            s += ceil((double)arr[i] / val); 
-        }
-        return (s <= k);
-    }
-    int smallestDivisor(vector<int>& arr, int k) {
-         int l = 1;
-        int maxi = *max_element(arr.begin(), arr.end());
-        int r = maxi;
-        int ans = -1;  
+bool isanswer(vector<int>&nums,int mid,int threshold){
+    int sum=0;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (iscorrect(mid, arr, k)) {
-                ans = mid;
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+    for(int i=0;i<nums.size();i++){
+        sum+=ceil((double)nums[i]/mid);
+    }
+    return sum<=threshold;
+}
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        //binary search on answers 
+        int l=1;
+        int ans;
+        int r=*max_element(nums.begin(),nums.end());
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isanswer(nums,mid,threshold)){
+                ans=mid;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
             }
         }
         return ans;
