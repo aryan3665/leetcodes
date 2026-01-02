188882f5042e4891ff491dc021ebf21742d016c6
aryan3665 <aryantripathi16022006@gmail.com>
2026-01-02 12:13:23 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (69.74%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_set<int>st;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(st.find(nums[i])!=st.end()){
+        //by observation jo repetede element hai uska index gar i hai to aisa ek condition jarur hoga jisme i-1 ya i-2 index pe same value hogi 
+        for(int i=1;i<n;i++){
+            if(nums[i]==nums[i-1]||((i!=1)&&(nums[i]==nums[i-2]))){
                 return nums[i];
             }
-            st.insert(nums[i]);
+            if(nums[n-1]==nums[0])return nums[0];
         }
         return -1;
-
     }
 };
