d6b983b83b6086820ee1e8c42596b9599c736495
aryan3665 <aryantripathi16022006@gmail.com>
2026-01-04 10:55:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.cpp
+++ Difficulty: Easy/Linked List Insertion At Beginning/linked-list-insertion-at-beginning.cpp
@@ -1,18 +1,28 @@
-// User function Template for C++
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
 
 class Solution {
   public:
-    // Function to insert a node at the begining of the linked list.
-    Node *insertAtBegining(Node *head, int x) {
+    Node *insertAtFront(Node *head, int x) {
         // Code here
+        if(head==NULL){
+            head=new Node(x);
+            return head;
+        }
+        
         Node*temp=new Node(x);
-        if(!head){
-            return temp;
-        }
-        else{
-            temp->next=head;
-            head=temp;
-        }
+        temp->next=head;
+        head=temp;
         return head;
     }
 };
