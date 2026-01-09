808064c1e802fd80feb46d29410ea0462451837e
aryan3665 <aryantripathi16022006@gmail.com>
2026-01-09 09:02:48 +0530

    Time: 388 ms (6.78%) | Memory: 288.1 MB (5.04%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
 
- bool allzero(vector<int>&freq){
-      for(int&it:freq){
-          if(it!=0)return false;
-      }
-      return true;
-  }
+bool allzero(vector<int>&fre,string p){
+    for(int i=0;i<p.size();i++){
+        if(fre[p[i]-'a']!=0)return false;
+    }
+    return true;
+}
     vector<int> findAnagrams(string s, string p) {
-         int i=0;
-        int j=0;
         int k=p.size();
-        int n=s.size();
-         vector<int>ans;
-        vector<int>freq(26,0);
+        vector<int>fre(26,0);
         for(int i=0;i<k;i++){
-            freq[p[i]-'a']++;
+            fre[p[i]-'a']++;
         }
-        
-        while(j<n){
-           
-                 freq[s[j]-'a']--;
-            
-           
-            
+        int i=0;
+        int j=0;
+        vector<int>ans;
+        while(j<s.size()){
+            fre[s[j]-'a']--;
+
             if(j-i+1==k){
-              if(allzero(freq))ans.push_back(i);
-                freq[s[i]-'a']++;
-                i++;
-                
+                if(allzero(fre,p)){
+                    ans.push_back(i);
+                }
+
+            
+                fre[s[i]-'a']++;
+                    i++;
             }
             j++;
-            
         }
-        
+
         return ans;
     }
 };
