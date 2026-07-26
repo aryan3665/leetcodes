da573b71050266c0dd48a8e22b8223414bf61c54
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-07-26 09:21:46 +0530

    Time: 13 ms (23.68%) | Memory: 33.9 MB (53.17%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,71 +1,53 @@
 class Solution {
 public:
-int r,c;
-    int row[4] = {0, 0, -1, 1};
-    int col[4] = {-1, 1, 0, 0};
+    int m, n;
+    int dir[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
 
-    bool valid(int i, int j) { return (i >= 0 && j >= 0 && i < r && j < c); }
-    int numEnclaves(vector<vector<int>>& board) {
-        int count = 0;
-        r = board.size();
-        c = board[0].size();
-        queue<pair<int, int>> q;
+    pair<bool,int> dfs(vector<vector<int>>& grid, int i, int j) {
 
-        // First row
-        for (int i = 0; i < c; i++) {
-            if (board[0][i] == 1) {
-                q.push({0, i});
-                board[0][i] = 0;
+        grid[i][j] = 0;   // visited mark
+
+        bool closed = true;
+        int cnt = 1;
+
+        if(i == 0 || i == m-1 || j == 0 || j == n-1)
+            closed = false;
+
+        for(auto &d : dir) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            if(ni >= 0 && ni < m && nj >= 0 && nj < n &&
+               grid[ni][nj] == 1) {
+
+                auto temp = dfs(grid, ni, nj);
+                closed = closed && temp.first;
+                cnt += temp.second;
             }
         }
 
-        // First column
-        for (int i = 0; i < r; i++) {
-            if (board[i][0] == 1) {
-                q.push({i, 0});
-                board[i][0] = 0;
-            }
-        }
+        return {closed, cnt};
+    }
 
-        // Last row
-        for (int i = 0; i < c; i++) {
-            if (board[r - 1][i] == 1) {
-                q.push({r - 1, i});
-                board[r - 1][i] = 0;
-            }
-        }
+    int numEnclaves(vector<vector<int>>& grid) {
 
-        // Last column (fixed bug here)
-        for (int i = 0; i < r; i++) {
-            if (board[i][c - 1] == 1) {
-                q.push({i, c - 1});
-                board[i][c - 1] = 0;
-            }
-        }
+        m = grid.size();
+        n = grid[0].size();
 
-        // BFS
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
+        int ans = 0;
 
-            for (int k = 0; k < 4; k++) {
-                int nx = x + row[k];
-                int ny = y + col[k];
-                if (valid(nx, ny) && board[nx][ny] == 1) {
-                    board[nx][ny] = 0;
-                    q.push({nx, ny});
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(grid[i][j] == 1) {
+                    auto res = dfs(grid, i, j);
+
+                    if(res.first)
+                        ans += res.second;
                 }
             }
         }
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (board[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-        return count;
+        return ans;
     }
 };
