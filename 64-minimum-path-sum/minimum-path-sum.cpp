aacf4d52246115b4a8867e63e6083f8d64c90e2b
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-07-27 11:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (72.07%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
-//     int dp[201][201];
-// #define INF 1e9
-
-// int solve(vector<vector<int>>& grid, int i, int j) {
-//     if (i == 0 && j == 0) return grid[0][0];
-//     if (i < 0 || j < 0) return INF;
-//     if (dp[i][j] != -1) return dp[i][j];
-
-//     int up = grid[i][j] + solve(grid, i - 1, j);
-//     int left = grid[i][j] + solve(grid, i, j - 1);
-//     return dp[i][j] = min(up, left);
-// }
-
-
-
     int minPathSum(vector<vector<int>>& grid) {
+
         int m = grid.size();
         int n = grid[0].size();
-     int dp[m][n];
-     for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0&&j==0)dp[0][0]=grid[0][0];
 
-            else{
-                int up=INT_MAX;
-                int left=INT_MAX;
+        // DP table
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-                if(i>0){
-                    up=grid[i][j]+dp[i-1][j];
+        // Starting cell
+        dp[0][0] = grid[0][0];
 
-                }
-                if(j>0){
-                    left=grid[i][j]+dp[i][j-1];
-                }
-                dp[i][j]=min(up,left);
+        // Puri DP table fill karenge
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                // Starting cell already fill hai
+                if(i == 0 && j == 0)
+                    continue;
+
+                int left = INT_MAX;
+                int up = INT_MAX;
+
+                // Agar left exist karta hai
+                if(j > 0)
+                    left = dp[i][j-1];
+
+                // Agar upper cell exist karta hai
+                if(i > 0)
+                    up = dp[i-1][j];
+
+                // Current cell ki value add kar do
+                dp[i][j] = grid[i][j] + min(left, up);
             }
         }
-     }
 
-     return dp[m-1][n-1];
+        // Destination ka minimum path sum
+        return dp[m-1][n-1];
     }
 };
