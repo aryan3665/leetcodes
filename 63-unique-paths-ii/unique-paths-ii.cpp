cb4aea4a3c02add6017fd4b40b47e75b3163dee6
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-07-27 11:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (32.02%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
 
-        int dp[100][100] = {0};
+        int m = grid.size();
+        int n = grid[0].size();
 
-        dp[0][0] = (obstacleGrid[0][0] == 1) ? 0 : 1;
+        // Agar start ya destination blocked hai
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1)
+            return 0;
 
-        // First row
-        for (int j = 1; j < n; j++) {
-            if (obstacleGrid[0][j] == 1)
-                dp[0][j] = 0;
-            else
-                dp[0][j] = dp[0][j - 1];
-        }
+        // DP table
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        // First column
-        for (int i = 1; i < m; i++) {
-            if (obstacleGrid[i][0] == 1)
-                dp[i][0] = 0;
-            else
-                dp[i][0] = dp[i - 1][0];
-        }
+        // Starting point
+        dp[0][0] = 1;
 
-        // Fill DP table
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] == 1)
+        // DP fill karenge
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                // Starting cell already fill hai
+                if(i == 0 && j == 0)
+                    continue;
+
+                // Agar obstacle hai to yaha koi path nahi aa sakta
+                if(grid[i][j] == 1)
+                {
                     dp[i][j] = 0;
-                else
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    continue;
+                }
+
+                int left = 0;
+                int up = 0;
+
+                // Left cell exist karta hai
+                if(j > 0)
+                    left = dp[i][j-1];
+
+                // Upper cell exist karta hai
+                if(i > 0)
+                    up = dp[i-1][j];
+
+                // Total paths
+                dp[i][j] = left + up;
             }
         }
 
-        return dp[m - 1][n - 1];
+        // Final answer
+        return dp[m-1][n-1];
     }
 };
