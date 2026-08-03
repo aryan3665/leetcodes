b4cf1fcdef3d11fa5bfef425247c53fe977ce6d6
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-08-03 07:16:29 +0530

    Time: 335 ms (5.09%) | Memory: 21.5 MB (99.84%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int t=0;
-        vector<bool>visited(nums.size()+1,0);
-        for(int i=0;i<nums.size();i++){
-            visited[nums[i]]=1;
+        // brute force
+        int n = nums.size();
+        int i = 0;
+        bool f = 0;
+        while (i <= n) {
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == i) {
+                    f = 1;
+                    break;
+                }
+            }
+
+            if (f == 0)
+                return i;
+            i++;
+            f=0;
         }
-        int ans=-1;
-        while(ans==-1){
-           if(visited[t]==0)ans=t;
-           else t++; 
-        }return ans;
+
+        return -1;
     }
 };
