48ab0cbbcab1f4a5bd3ae669be8ab27c2526b86b
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-08-03 08:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (36.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       int result=-1;
-       int low=0;
-       int n=nums.size();
-       int high=n-1;
-       while(low<=high){
-        int mid=low+(high-low)/2;
-        if(nums[mid]==target)return mid;
-        //check kya left half sorted hai
-        if(nums[low]<=nums[mid]){
-            //check karo ki target uss sorted half me hai
-            if(nums[low]<=target&&target<=nums[mid]){
-                high=mid-1;
+        int l=0;int r=nums.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+
+            if(nums[mid]==target)return mid;
+
+            if(nums[l]<=nums[mid]){
+                if(target>=nums[l]&&target<=nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
             }
+
             else{
-                low=mid+1;
+                if(target>=nums[mid]&&target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
             }
         }
-
-        else{
-            //mATlab right half sorted hai ek half to sorted hoga hi 
-
-            //check karenge ki value kis part me hai 
-            if(nums[mid]<=target&&target<=nums[high]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-       }
-
-       return -1; 
+        return -1;
     }
 };
