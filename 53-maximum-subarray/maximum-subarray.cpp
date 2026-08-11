472e7a8252e02f943b1fc1929b6585d5db535955
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-08-11 14:37:35 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (98.87%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = INT_MIN;
-//kadens chacha algorith start karte hain yaha pe
-int sum=0;
-for(int i=0;i<n;i++){
-    if(sum<0)sum=0;
-    sum+=nums[i];
-    maxi=max(sum,maxi);
+        int current = nums[0];
+        int ans = nums[0];
 
-}
-return maxi;
+        for(int i = 1; i < nums.size(); i++) {
+
+            // Choice:
+            // 1. New subarray start karo
+            // 2. Previous subarray continue karo
+            current = max(nums[i], current + nums[i]);
+
+            ans = max(ans, current);
+        }
+
+        return ans;
     }
 };
