6d3ba1dbd6ea1b72950e8c0144a6c3972ced76f2
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-09-05 21:40:25 +0530

    Time: 11 ms (57.63%) | Memory: 202.8 MB (67.90%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        // vector<int> maxt(n);
-        vector<int> minit(n);
-        // maxt[0] = nums[0];
-        // for (int i = 1; i < n; i++) {
-        //     maxt[i] = max(maxt[i - 1], nums[i]);
-        // }
-        minit[n - 1] = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            minit[i] = min(minit[i + 1], nums[i]);
+        vector<int> minValue(n);
+        minValue[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            minValue[i] = min(minValue[i + 1], nums[i]);
         }
-        int ans = -1;
-        int maxt=nums[0];
 
-        for (int i = 0; i < n; i++) {
-            maxt=max(maxt,nums[i]);
-            if ((maxt - minit[i]) <= k) {
-                ans = i;
-                break;
+        int maxValue = 0;
+        for (int i = 0; i < n; ++i) {
+            maxValue = max(maxValue, nums[i]);
+            if (maxValue - minValue[i] <= k) {
+                return i;
             }
         }
-
-        return ans;
+        return -1;
     }
 };
