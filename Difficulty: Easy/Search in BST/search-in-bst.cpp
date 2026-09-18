fedad32d6b29e500455b7d758d6209fffd527e40
Aryan Tripathi | MERN & DSA <aryantripathi16022006@gmail.com>
2026-09-18 08:27:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in BST/search-in-bst.cpp
@@ -0,0 +1,26 @@
+/* Definition for Node
+class Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    bool search(Node* root, int key) {
+        // code here
+        if(!root)return 0;
+        if(root->data==key)return 1;
+        
+        if(root->data>key){
+            return search(root->left,key);
+        }
+        return search(root->right,key);
+    }
+};
